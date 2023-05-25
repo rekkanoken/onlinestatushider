d6d149726093549594190c398286a7c204a46eed
rekka <134650483+rekkanoken@users.noreply.github.com>
2023-05-25 13:51:47 -0700

    Add files via upload

---
+++ onlinestatushider.js
@@ -0,0 +1,37 @@
+META{"name":"OnlineStatusHiderPlugin","rekka":"Online Status Hider Plugin","website":"https://twitter.com/rekkan0ken"}*//
+
+class OnlineStatusHiderPlugin {
+    getName() {
+        return "Online Status Hider Plugin";
+    }
+
+    getDescription() {
+        return "Hides all online statuses on Discord.";
+    }
+
+    start() {
+        this.hideOnlineStatuses();
+    }
+
+    stop() {
+        this.restoreOnlineStatuses();
+    }
+
+    hideOnlineStatuses() {
+        const styleElement = document.createElement("style");
+        styleElement.setAttribute("id", "online-status-hider-plugin-style");
+        styleElement.textContent = `
+            .member .status {
+                display: none !important;
+            }
+        `;
+        document.head.appendChild(styleElement);
+    }
+
+    restoreOnlineStatuses() {
+        const styleElement = document.getElementById("online-status-hider-plugin-style");
+        if (styleElement) {
+            styleElement.remove();
+        }
+    }
+}
